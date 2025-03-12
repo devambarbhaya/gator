a8fea2c94dd33a4e3e4c1e8e3269017672d96cae
devambarbhaya <barbhaya.devam@gmail.com>
2025-03-12 14:32:34 +0000

    add commands

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"gator/internal/config"
 	"log"
+	"os"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main()  {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("devam")
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	programState := &state{
+		cfg: &cfg,
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+
+	cmds := commands {
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
