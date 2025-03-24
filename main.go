e98abfe7e016f5b0892a59c0c352475e567bc033
devambarbhaya <barbhaya.devam@gmail.com>
2025-03-24 18:52:25 +0000

    add SQLC and register command

--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-	"gator/internal/config"
+	"database/sql"
 	"log"
 	"os"
+
+	"gator/internal/config"
+	"gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
-func main()  {
+func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
-	cmds := commands {
+	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
-		log.Fatalf("Usage: cli <command> [args...]")
+		log.Fatal("Usage: cli <command> [args...]")
 		return
 	}
 
